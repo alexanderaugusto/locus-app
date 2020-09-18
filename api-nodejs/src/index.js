a464edc8a485b58f5e25a5b36c1033190b0dcabe
Alexander Augusto <alexaasf_10@hotmail.com>
2020-09-18 14:49:53 -0300

    configure http server with express (routes and app)

---
+++ api-nodejs/src/index.js
@@ -0,0 +1,11 @@
+const http = require('http')
+
+const app = require('./app')
+
+const httpServer = http.createServer(app)
+const server = httpServer.listen(process.env.PORT || 5000, () => {
+  const host = server.address().address
+  const port = server.address().port
+
+  console.log("Server is running on http://%s:%s", host, port)
+})
