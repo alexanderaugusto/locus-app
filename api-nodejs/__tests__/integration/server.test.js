482740e7ac3132152aa7466668a36eedeb64b8a0
Alexander Augusto <alexaasf_10@hotmail.com>
2020-09-19 16:51:08 -0300

    create integration test to test express connection

---
+++ api-nodejs/__tests__/integration/server.test.js
@@ -0,0 +1,11 @@
+const supertest = require('supertest')
+const request = require('supertest')
+const app = require('../../src/app')
+
+describe("Server test", () => {
+  it("Should connect to server and return 404", async () => {
+    const response = await request(app).get("/")
+
+    expect(response.status).toBe(404)
+  })
+})
