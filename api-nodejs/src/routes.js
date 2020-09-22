5b21b27bb09ba3b134f03b6a3c9a582c2e489fb8
Alexander Augusto <alexaasf_10@hotmail.com>
2020-09-22 12:46:50 -0300

    add and configure multer.js to project to save files

--- api-nodejs/src/routes.js
+++ api-nodejs/src/routes.js
@@ -1,13 +1,15 @@
 const routes = require('express').Router()
+const multer = require('multer')
+const multerConfig = require('./config/multer')
 
 const UserController = require('./controllers/UserController')
 
-routes.post('/user', UserController.create)
+routes.post('/user', multer(multerConfig('user')).single('file'), UserController.create)
 routes.get('/user/:user_id', UserController.list)
 routes.get('/user/:user_id/properties', UserController.list_properties)
 routes.get('/user/:user_id/rentals', UserController.list_rentals)
 routes.get('/user/:user_id/favorites', UserController.list_favorites)
-routes.put('/user/:user_id', UserController.update)
+routes.put('/user/:user_id', multer(multerConfig('user')).single('file'), UserController.update)
 routes.delete('/user/:user_id', UserController.delete)
 
 module.exports = routes
