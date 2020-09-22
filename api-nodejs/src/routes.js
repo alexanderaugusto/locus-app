45cbcff203f2d65649149536c3b5175ee0affe02
Alexander Augusto <alexaasf_10@hotmail.com>
2020-09-22 13:05:33 -0300

    change multerConfig folder parameter user -> /user

--- api-nodejs/src/routes.js
+++ api-nodejs/src/routes.js
@@ -4,12 +4,12 @@ const multerConfig = require('./config/multer')
 
 const UserController = require('./controllers/UserController')
 
-routes.post('/user', multer(multerConfig('user')).single('file'), UserController.create)
+routes.post('/user', multer(multerConfig('/user')).single('file'), UserController.create)
 routes.get('/user/:user_id', UserController.list)
 routes.get('/user/:user_id/properties', UserController.list_properties)
 routes.get('/user/:user_id/rentals', UserController.list_rentals)
 routes.get('/user/:user_id/favorites', UserController.list_favorites)
-routes.put('/user/:user_id', multer(multerConfig('user')).single('file'), UserController.update)
+routes.put('/user/:user_id', multer(multerConfig('/user')).single('file'), UserController.update)
 routes.delete('/user/:user_id', UserController.delete)
 
 module.exports = routes
