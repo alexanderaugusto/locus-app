c305f63aeef5d0bae9ad6d7661f83ea642435197
Alexander Augusto <alexaasf_10@hotmail.com>
2020-09-22 09:56:30 -0300

    create user routes and UserController

---
+++ api-nodejs/src/controllers/UserController.js
@@ -0,0 +1,139 @@
+const { User } = require('../models')
+
+module.exports = {
+  create: async (req, rest) => {
+    const { email, password, name, cpf, phone } = req.body
+    const { key: avatar } = req.file || { key: 'default-avatar.png' }
+
+    User.findOrCreate({
+      where: { email },
+      defaults: { email, password, name, cpf, phone, avatar }
+    })
+      .then(([user, created]) => {
+        if (!created) {
+          return res.status(409).json({
+            cod: 409,
+            msg: 'Este email já foi cadastrado anteriormente.'
+          })
+        }
+
+        return res.json(user)
+      })
+      .catch((err) => {
+        return res.status(500).json({
+          cod: 500,
+          msg: 'Ocorreu um erro inesperado ao cadastrar o usuário. Por favor, tentar novamente.'
+        })
+      })
+  },
+
+  list: async (req, res) => {
+    const { user_id } = req.params
+
+    User.findByPk(user_id)
+      .then((user) => {
+        return res.json(user)
+      })
+      .catch((err) => {
+        return res.status(500).json({
+          cod: 500,
+          msg: 'Ocorreu um erro inesperado ao listar o usuário. Por favor, tentar novamente.'
+        })
+      })
+  },
+
+  list_properties: async (req, res) => {
+    const { user_id: id } = req.params
+
+    User.findByPk(id, { include: { association: 'properties' } })
+      .then((user) => {
+        return res.json(user.properties)
+      })
+      .catch((err) => {
+        return res.status(500).json({
+          cod: 500,
+          msg: 'Ocorreu um erro inesperado ao listar as propriedades do usuário. Por favor, tentar novamente.'
+        })
+      })
+  },
+
+  list_rentals: async (req, res) => {
+    const { user_id: id } = req.params
+
+    User.findByPk(id, { include: { association: 'rentals' } })
+      .then((user) => {
+        return res.json(user.rentals)
+      })
+      .catch((err) => {
+        return res.status(500).json({
+          cod: 500,
+          msg: 'Ocorreu um erro inesperado ao listar os aluguéis do usuário. Por favor, tentar novamente.'
+        })
+      })
+  },
+
+  list_favorites: async (req, res) => {
+    const { user_id: id } = req.params
+
+    User.findByPk(id, { include: { association: 'favorites' } })
+      .then((user) => {
+        return res.json(user.favorites)
+      })
+      .catch((err) => {
+        return res.status(500).json({
+          cod: 500,
+          msg: 'Ocorreu um erro inesperado ao listar as propriedades favoritadas pelo usuário. Por favor, tentar novamente.'
+        })
+      })
+  },
+
+  update: async (req, res) => {
+    const { user_id: id } = req.params
+    const { name, cpf, phone } = req.body
+    const { key: avatar } = req.file || { key: undefined }
+
+    User.update({ name, cpf, phone, avatar }, { where: { id } })
+      .then(([updated]) => {
+        if (!updated) {
+          return res.status(400).json({
+            cod: 400,
+            message: 'Os dados fornecidos são inválidos. Por favor, verifique os dados enviados e tente novamente.'
+          })
+        }
+
+        if (avatar) {
+          return res.json({ avatar })
+        }
+
+        return res.status(204).json()
+      })
+      .catch((err) => {
+        return res.status(500).json({
+          cod: 500,
+          msg: 'Ocorreu um erro inesperado ao atualizar o usuário. Por favor, tentar novamente.'
+        })
+      })
+  },
+  
+  delete: async (req, res) => {
+    const { user_id: id } = req.params
+
+    User.destroy({ where: { id } })
+      .then((deleted) => {
+        if (!deleted) {
+          return res.status(400).json({
+            cod: 400,
+            message: 'Não conseguimos apagar este usuário. Por favor, tente novamente'
+          })
+        }
+
+        return res.status(204).json()
+      })
+      .catch((err) => {
+        return res.status(500).json({
+          cod: 500,
+          msg: 'Ocorreu um erro inesperado ao atualizar o usuário. Por favor, tentar novamente.'
+        })
+      })
+  }
+}
--- api-nodejs/src/routes.js
+++ api-nodejs/src/routes.js
@@ -1,3 +1,13 @@
 const routes = require('express').Router()
 
+const UserController = require('./controllers/UserController')
+
+routes.post('/user', UserController.create)
+routes.get('/user/:user_id', UserController.list)
+routes.get('/user/:user_id/properties', UserController.list_properties)
+routes.get('/user/:user_id/rentals', UserController.list_rentals)
+routes.get('/user/:user_id/favorites', UserController.list_favorites)
+routes.put('/user/:user_id', UserController.update)
+routes.delete('/user/:user_id', UserController.delete)
+
 module.exports = routes
