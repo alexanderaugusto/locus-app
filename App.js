fbdc9aeb759e865f9bd317e0ce2fc88d6f7c0934
Alexander Augusto <alexaasf_10@hotmail.com>
2020-10-18 14:23:46 -0300

    added StatusBar component for android

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { StatusBar } from 'react-native'
+import colors from './src/constants/colors.json'
 import Routes from './src/routes'
 
 export default function App() {
   return (
-    <Routes />
+    <>
+      <StatusBar barStyle="light-content" backgroundColor={colors["blue"]} />
+      <Routes />
+    </>
   )
 }
 
