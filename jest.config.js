c9d8ddf67a0d1a5538cb786923b5c6c8e0ca6c76
Alexander Augusto <alexaasf_10@hotmail.com>
2020-10-30 11:39:51 -0300

    ignore jest coverage after test

--- jest.config.js
+++ jest.config.js
@@ -5,9 +5,9 @@ module.exports = {
   collectCoverageFrom: ["src/**"],
   coverageProvider: "v8",
   testEnvironment: "node",
-  // testMatch: [
-  //   "**/__tests__/**/*.test.js?(x)"
-  // ],
+  testMatch: [
+    "**/__tests__/**/*.test.js?(x)"
+  ],
   preset: "jest-expo",
   transformIgnorePatterns: [
     "node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)"
