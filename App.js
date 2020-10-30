36bb4db6295f0b59a13fad11e976739d7dba2776
Alexander Augusto <alexaasf_10@hotmail.com>
2020-10-30 17:37:25 -0300

    added updates over the air

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { StatusBar } from 'react-native'
+import * as Updates from 'expo-updates'
 import colors from './src/constants/colors.json'
 import Routes from './src/routes'
 
 export default function App() {
+  useEffect(() => {
+    async function updateApp() {
+      const { isAvailable } = await Updates.checkForUpdateAsync()
+
+      if (isAvailable) {
+        await Updates.fetchUpdateAsync()
+        await Updates.reloadAsync()
+      }
+    }
+
+    updateApp()
+  }, [])
+
   return (
     <>
-      <StatusBar barStyle="light-content" backgroundColor={colors["blue"]} />
+      <StatusBar barStyle="light-content" backgroundColor={colors.blue} />
       <Routes />
     </>
   )
 }
-
