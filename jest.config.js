51621192b82543149c74914f6fb0a253a5ac5a12
Alexander Augusto <alexaasf_10@hotmail.com>
2020-10-30 13:14:08 -0300

    change on jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,15 +2,13 @@ module.exports = {
   bail: true,
   clearMocks: true,
   collectCoverage: true,
-  coverageDirectory: "__tests__/coverage",
-  collectCoverageFrom: ["src/**"],
-  coverageProvider: "v8",
-  testEnvironment: "node",
-  testMatch: [
-    "**/__tests__/**/*.test.js?(x)"
-  ],
-  preset: "jest-expo",
+  coverageDirectory: '__tests__/coverage',
+  collectCoverageFrom: ['src/**'],
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.js?(x)'],
+  preset: 'jest-expo',
   transformIgnorePatterns: [
-    "node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)"
+    'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)'
   ]
 }
