39744ca79ad78a9e42d93051829d6e47b8413bcf
Alexander Augusto <alexaasf_10@hotmail.com>
2020-11-08 08:54:41 -0300

    request camera inside pickImage function

--- src/components/ImagePicker.js
+++ src/components/ImagePicker.js
@@ -1,20 +1,16 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { TouchableOpacity, Platform } from 'react-native'
 import * as ImagePicker from 'expo-image-picker'
 
 export default function ImagePickerFunction({ onChange, children, ...props }) {
-  useEffect(() => {
-    ;(async () => {
-      if (Platform.OS !== 'web') {
-        const { status } = await ImagePicker.requestCameraRollPermissionsAsync()
-        if (status !== 'granted') {
-          alert('Para prosseguir, é necessário permitir o acesso a câmera!')
-        }
-      }
-    })()
-  }, [])
-
   const pickImage = async () => {
+    if (Platform.OS !== 'web') {
+      const { status } = await ImagePicker.requestCameraRollPermissionsAsync()
+      if (status !== 'granted') {
+        alert('Para prosseguir, é necessário permitir o acesso a câmera!')
+      }
+    }
+
     const result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
