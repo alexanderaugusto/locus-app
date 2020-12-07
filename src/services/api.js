1389739a15b40f97e8bb4dca43f194e508713796
Alexander Augusto <alexaasf_10@hotmail.com>
2020-12-06 22:30:17 -0300

    return api to heroku

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.172:5000'
+  baseURL: 'https://imovel-api.herokuapp.com'
 })
 
 const STORAGE_URL = 'https://res.cloudinary.com/imovel/image/upload/v1602331961'
